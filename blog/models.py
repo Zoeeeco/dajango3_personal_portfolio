d4a361ab99e8c1ea0d2dc4d09bd6cc6b583bb4dc
Zixuan Jing <zoe.zixuanjing@gmail.com>
2022-03-29 01:18:23 +1100

    local settings

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class Blog(models.Model):
     tittle = models.CharField(max_length=200)
     description = models.TextField()
+    #新添加：
+    image = models.ImageField(upload_to='portfolio/images/')
     date = models.DateField()
     
    
